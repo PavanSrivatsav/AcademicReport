83c360b840425d368821a671439e9190c4e4cd57
vikkysri77 <vikkysri77>
2017-03-08 12:46:16 +0530

    updated with projectSprintActivityDTO

--- src/main/java/com/revature/data/impl/ProjectSprintActivityDAOImpl.java
+++ src/main/java/com/revature/data/impl/ProjectSprintActivityDAOImpl.java
@@ -12,6 +12,7 @@ import com.revature.data.access.exception.DataAccessException;
 import com.revature.data.exception.DataServiceException;
 import com.revature.data.utils.DataUtils;
 import com.revature.model.ProjectSprintActivity;
+import com.revature.model.dto.ProjectSprintActivityDTO;
 
 @Repository
 public class ProjectSprintActivityDAOImpl implements ProjectSprintActivityDAO {
@@ -29,12 +30,12 @@ public class ProjectSprintActivityDAOImpl implements ProjectSprintActivityDAO {
 	}
 
 	@Override
-	public List<ProjectSprintActivity> getAllProjectSprintActivities() throws DataServiceException {
-		List<ProjectSprintActivity> projectSprintActivities = null;
+	public List<ProjectSprintActivityDTO> getAllProjectSprintActivities() throws DataServiceException {
+		List<ProjectSprintActivityDTO> projectSprintActivities = null;
 		try {
-			StringBuilder sb = new StringBuilder("select * from project_sprint_activities p where p.IS_ACTIVE=true");
-			projectSprintActivities = dataRetriver.retrieveBySQLAsJSONInDAO(sb.toString());
-			logger.info("ProjectSprintActivitys data retrieval success..");
+			StringBuilder sb = new StringBuilder("select p.ID id,p.PROJECT_SPRINT_ID projectSprintId,p.VIDEO_ID videoId,p.COURSE_ID courseId,p.QUIZ_ID quizId,p.ONLINE_ACTIVITY onlineActivity,p.OFFLINE_ACTIVITY offlineActivity,p.DURATION_IN_MINUTES duration from project_sprint_activities p where p.IS_ACTIVE=true");
+			projectSprintActivities =dataRetriver.retrieveBySQLAsJSON(sb.toString(),ProjectSprintActivityDTO.class);
+			logger.info("ProjectSprintActivity data retrieval success..");
 		} catch (DataAccessException e) {
 			logger.error(e.getMessage(), e);
 			throw new DataServiceException(DataUtils.getPropertyMessage("data_retrieval_fail"), e);
@@ -43,8 +44,17 @@ public class ProjectSprintActivityDAOImpl implements ProjectSprintActivityDAO {
 	}
 
 	@Override
-	public List<ProjectSprintActivity> getProjectSprintActivityById(int projectSprintActivityId)
-			throws DataServiceException {
-		return null;
+	public <E> ProjectSprintActivityDTO getProjectSprintActivityById(ProjectSprintActivity projectSprintActivity) throws DataServiceException {
+		ProjectSprintActivityDTO projectSprintActivityById = null;
+		try {
+			StringBuilder sb = new StringBuilder(
+					"SELECT p.ID id,p.PROJECT_SPRINT_ID projectSprintId,p.VIDEO_ID videoId,p.COURSE_ID courseId,p.QUIZ_ID quizId,p.ONLINE_ACTIVITY onlineActivity,p.OFFLINE_ACTIVITY offlineActivity,p.DURATION_IN_MINUTES duration FROM project_sprint_activities p WHERE ID="+ projectSprintActivity.getId() + " and p.IS_ACTIVE=true");
+			projectSprintActivityById = (ProjectSprintActivityDTO)dataRetriver.retrieveBySQLAsObject(sb.toString(), ProjectSprintActivityDTO.class);
+			logger.info("projectSprintActivity  by id data retrieval success..");
+		} catch (DataAccessException e) {
+			logger.error(e.getMessage(), e);
+			throw new DataServiceException(DataUtils.getPropertyMessage("data_retrieval_fail"), e);
+		}
+		return projectSprintActivityById;
 	}
 }
